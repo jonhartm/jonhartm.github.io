02870d828680f9e889e3dd74040d910d4cb1c74e
jonhartm <jonhartm@umich.edu>
2018-04-04 21:12:11 -0400

    restricted project filter,pictures,and on hover css to desktop. Also added links to project details

--- js/project_desc.js
+++ js/project_desc.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
   $(".project_container").hover(function() {
-    $("p", this).show("fade", 500);
-    $("img", this).fadeTo(500, 0.3);
+    if ($(window).width > 1000) {
+      $("p", this).show("fade", 500);
+      $("img", this).fadeTo(500, 0.3);
+    }
   }, function() {
-    $("p", this).hide("fade", 500);
-    $("img", this).fadeTo(500, 1);
+    if ($(window).width > 1000) {
+      $("p", this).hide("fade", 500);
+      $("img", this).fadeTo(500, 1);
+    }
   });
 });
