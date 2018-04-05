633f60c968dad9b58ee797a6eece4a071a374d00
jonhartm <jonhartm@umich.edu>
2018-04-04 23:12:37 -0400

    redid contact icons for table/desktop views

--- js/nav.js
+++ js/nav.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
   $(".fa-bars").click(function() {
     $("#nav-bar").toggle("blind", 200);
   });
+
+  // set the FA icons for large views
+  if ($(window).width() > 480) {
+    $(".fa-github, .fa-envelope").addClass("fa-2x");
+  }
 });
