696817fecae34506a333754e0fcab68271fb44c1
jonhartm <jonhartm@umich.edu>
2018-04-06 02:25:50 -0400

    I don't understand but this works

--- js/image_slideshow.js
+++ js/image_slideshow.js
@@ -1,18 +1,21 @@
 $(document).ready(function() {
   $('.project').hover(function(x){
     var images = $(this).find(".image-container").children("img");
+    $(images).not(".active").css({"display":"none"});
     var active_index = -1;
     window.timeout = setInterval(function() {
       for (var i = 0; i < images.length; i++) {
         if ($(images[i]).hasClass("active")) {
           active_index = i;
           $(images[i]).toggleClass("active");
+          $(images[i]).fadeToggle().css({"display":"none"});
         }
       }
       // mod math to loop back
       active_index = ++active_index % images.length;
       $(images[active_index]).toggleClass("active");
-    }, 2000);
+      $(images[active_index]).fadeToggle();
+    }, 3000);
   }, function() {
     clearTimeout(window.timeout);
   });
