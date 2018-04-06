385342104f6cec19ffb7631ffc9398a77d019428
jonhartm <jonhartm@umich.edu>
2018-04-06 13:31:09 -0400

    restored the jquery effect on project hover

--- js/project_desc.js
+++ js/project_desc.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  $(".project_container").hover(function() {
-    if ($(window).width > 1000) {
+  $(".project-container").hover(function() {
+    if ($(window).width() > 1000) {
       $("p", this).show("fade", 500);
-      $("img", this).fadeTo(500, 0.3);
+      $("img", this).fadeTo(500, 0.1);
     }
   }, function() {
-    if ($(window).width > 1000) {
+    if ($(window).width() > 1000) {
       $("p", this).hide("fade", 500);
       $("img", this).fadeTo(500, 1);
     }
