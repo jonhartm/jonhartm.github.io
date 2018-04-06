f4a3aa5882f186f42b25660394083308d4a9d9a3
jonhartm <jonhartm@umich.edu>
2018-04-06 03:03:54 -0400

    added captions for project detail images

--- js/image_slideshow.js
+++ js/image_slideshow.js
@@ -1,23 +1,29 @@
 $(document).ready(function() {
-  $('.project').hover(function(x){
+  $('.project').hover(function() {
+    console.log("starting");
     var images = $(this).find(".image-container").children("img");
-    $(images).not(".active").css({"display":"none"});
-    var active_index = -1;
-    window.timeout = setInterval(function() {
-      for (var i = 0; i < images.length; i++) {
-        if ($(images[i]).hasClass("active")) {
-          active_index = i;
-          $(images[i]).toggleClass("active");
-          $(images[i]).fadeToggle().css({"display":"none"});
+    var cap = $(this).find(".image-container").children(".caption");
+    if (images.length > 1) { // don't bother unless there's at least two pictures
+      $(images).not(".active").css({"display":"none"});
+      var active_index = -1;
+      window.timeout = setInterval(function() {
+        for (var i = 0; i < images.length; i++) {
+          if ($(images[i]).hasClass("active")) {
+            active_index = i;
+            $(images[i]).toggleClass("active");
+            $(images[i]).fadeToggle().css({"display":"none"});
+          }
         }
-      }
-      // mod math to loop back
-      active_index = ++active_index % images.length;
-      $(images[active_index]).toggleClass("active");
-      $(images[active_index]).fadeToggle();
-    }, 3000);
+        // mod math to loop back
+        active_index = ++active_index % images.length;
+        $(images[active_index]).toggleClass("active");
+        $(images[active_index]).fadeToggle();
+        var img_alt_text = $(images[active_index]).attr("alt");
+        $(cap).html(img_alt_text);
+      }, 5000);
+    }
   }, function() {
-    clearTimeout(window.timeout);
+    clearTimeout(window.timeout); // clear the timer
   });
 });
 
